d7fea29c0c8de04bcf9be2b1898b8b5188c85f6e
Pratham <pratham.patelpr@gmail.com>
2025-12-08 23:19:08 -0500

    Removing Magnum

--- backend/server/main.py
+++ backend/server/main.py
@@ -3,7 +3,6 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.middleware.trustedhost import TrustedHostMiddleware
 from sqlalchemy import text
-from mangum import Mangum
 
 from server.api import auth, stocks
 from server.utils.database import init_database, close_db_connection, SessionLocal
@@ -97,4 +96,3 @@ def health_check():
         db.close()
 
 
-handler = Mangum(app)
